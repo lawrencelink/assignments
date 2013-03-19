54f721ec15d7f617fa20dfe2fcf660999a987014
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-19 16:38:04 -0400

    red condition

--- homework-9.1.js
+++ homework-9.1.js
@@ -15,4 +15,9 @@ var assert = require("assert");
  *  +0 | -1  | -1
  */
  
+function addition(x,y) {
+    return 2;
+}
 
+assert(addition(1,1)===2, '1+1 does equal 2');
+assert(addition(1,0)===1, '1+0 does equal 1');
