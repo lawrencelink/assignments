7a04d0cc1faec229770f76475aaa9c6d08359bf3
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-19 17:00:58 -0400

    ran out of time

--- homework-9.1.js
+++ homework-9.1.js
@@ -4,20 +4,23 @@ var assert = require("assert");
  * testing subtraction():
  * one | two | out
  * ====+=====+====
- *  +1 | +1  | +2
- *  +1 | +0  | +1
- *  +1 | -1  | +0
- *  -1 | +1  | +0
- *  -1 | +0  | -1
- *  -1 | -1  | -2
- *  +0 | +1  | +1
+ *  +1 | +1  | 0
+ *  +1 | +0  | 1
+ *  +1 | -1  | 2
+ *  -1 | +1  | -2
+ *  -1 | +0  | -2
+ *  -1 | -1  | 0
+ *  +0 | +1  | -1
  *  +0 | +0  | +0
- *  +0 | -1  | -1
+ *  +0 | -1  | 0
  */
  
-function addition(x,y) {
-    return 2;
+function subtraction(x,y) {
+    z = x - y;
+    return z;
 }
 
-assert(addition(1,1)===2, '1+1 does equal 2');
-assert(addition(1,0)===1, '1+0 does equal 1');
+
+
+assert(subtraction(1,1)===0,'1-1 does equal 0');
+assert(subtraction(1,0)===1,'1-0 does equal 1');
