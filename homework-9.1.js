a0438a779d50bc7e918ec19c916ba8ff0070eb9a
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-19 16:22:30 -0400

    starting from the beginning

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 
 /**
- * testing addition():
+ * testing subtraction():
  * one | two | out
  * ====+=====+====
  *  +1 | +1  | +2
@@ -15,16 +15,4 @@ var assert = require("assert");
  *  +0 | -1  | -1
  */
  
-var addition = function(a, b) {
-     return (a+b);
-}
- 
-assert(addition(1,1) === 2, '1 + 1 does equal 2');
-assert(addition(1,0) === 1, '1 + 1 does equal 2');
-assert(addition(1,-1)===0, '1 - 1 does equal 0');
-assert(addition(-1,1)===0,'-1+1 does equal 0');
-assert(addition(-1,0)===-1,'-1+0 does equal -1');
-assert(addition(-1,-1)===-2,'-1-1 does equal -2');
-assert(addition(0,1)===1,'0+1 does equal 1');
-assert(addition(0,0)===0,'0+0 does equal 0');
-assert(addition(0,-1)===-1, '0-1 does equal -1');
+
