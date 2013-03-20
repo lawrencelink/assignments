2e78734c8f212f09545e9bfa6ffc4ae7967ae92b
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-20 17:57:57 -0400

    starting at the beginning of multiplication

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,31 +1,23 @@
 var assert = require("assert");
 
 /**
- * testing subtraction():
+ * testing division():
  * one | two | out
  * ====+=====+====
- *  +1 | +1  | 0
- *  +1 | +0  | 1
- *  +1 | -1  | 2
- *  -1 | +1  | -2
- *  -1 | +0  | -1
- *  -1 | -1  | 0
- *  +0 | +1  | -1
- *  +0 | +0  | +0
+ *  +1 | +1  | 1
+ *  +1 | +0  | 0
+ *  +1 | -1  | -1
+ *  -1 | +1  | -1
+ *  -1 | +0  | 0
+ *  -1 | -1  | 1
+ *  +0 | +1  | 0
+ *  +0 | +0  | 0
  *  +0 | -1  | 0
  */
  
-function subtraction(x,y) {
-    z = x - y;
+function division(x,y) {
+    
     return z;
 }
 
-assert(subtraction(1,1)===0,"1-1 does equal 0");
-assert(subtraction(1,0)===1,"1-0 does equal 1");
-assert(subtraction(1,-1)===2,"1+(-1) does equal 2");
-assert(subtraction(-1,1)===-2,"-1-1 does equal -2");
-assert(subtraction(-1,0)===-1,"-1+0 does equal -1");
-assert(subtraction(-1,-1)===0,"-1+(-1) does equal 0");
-assert(subtraction(0,1)===-1,"0-1 does equal 1");
-assert(subtraction(0,0)===0,"0-0 does equal 0");
-assert(subtraction(0,-1),"0-1 does equal -1");
+
