76ed634e9010c5361628ece7532f27e4f6ceb960
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-21 17:30:38 -0400

    starting at division today

--- homework-9.1.js
+++ homework-9.1.js
@@ -1,32 +1,24 @@
 var assert = require("assert");
 
 /**
- * testing multiplication():
+ * testing division():
  * one | two | out
  * ====+=====+====
  *  +1 | +1  | 1
- *  +1 | +0  | 0
+ *  +1 | +0  | NaN
  *  +1 | -1  | -1
  *  -1 | +1  | -1
- *  -1 | +0  | 0
+ *  -1 | +0  | NaN
  *  -1 | -1  | 1
  *  +0 | +1  | 0
- *  +0 | +0  | 0
+ *  +0 | +0  | NaN
  *  +0 | -1  | 0
  */
  
-function multiplication(x,y) {
-    z = (x*y);
+function division(x,y) {
+    z = (x/y);
     return z;
 }
 
-assert(multiplication(1,1)===1,"1*1 does equal 1");
-assert(multiplication(1,0)===0,"1*0 does equal 0");
-assert(multiplication(1,-1===-1,"1*-1 does equal -1"));
-assert(multiplication(-1,1)===-1,"-1*1 does equal 1");
-assert(multiplication(-1,0)===0,"-1*0 does equal 0");
-assert(multiplication(-1,-1)===1,"-1*-1 does equal 1");
-assert(multiplication(0,1)===0,"0*1 does equal 0");
-assert(multiplication(0,0)===0,"0*0 does equal 0");
-assert(multiplication(0,-1)===0, "0*-1 does equal 0");
+
 
