59db7974a16dc6d77284fbc3199c2683f95b1d08
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-25 17:56:40 -0400

    Red

--- homework-10.1.js
+++ homework-10.1.js
@@ -15,4 +15,9 @@ var assert = require("assert");
  *  +0 | -1  | -1
  */
  
+ assert.equal( +2, addition(+1,+1), " +1 +1 = +2");
+ assert.equal( +1, addition(+1,+0), " +1 +0 = +1");
  
+ function addition(a,b) {
+     return 2;
+}
