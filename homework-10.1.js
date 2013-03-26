3d8948c2b87d65a110a9f436e6290a6677ce0302
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-26 09:17:12 -0400

    Refactor

--- homework-10.1.js
+++ homework-10.1.js
@@ -15,9 +15,18 @@ var assert = require("assert");
  *  +0 | -1  | +1
  */
 
-assert.equal( +0,subtraction(+1,+1), " +1 - +1 = +0");
-assert.equal( +1, subtraction(+1,+0), "+1 - +0 = +1");
+assert.equal( +0, subtraction(+1,+1), " +1 - +1 = +0");
+assert.equal( +1, subtraction(+1,+0), " +1 - +0 = +1");
+autoAssert( +1, -1, +2);
 
-function subtraction(a,b) {
+function autoAssert( a, b, c) {
+    
+    assert.equal(c, subtraction( a, b), " +1 - -1 = +2");
+    
+}
+
+function subtraction(a, b) {
+    
     return (a - b);
+    
 }
