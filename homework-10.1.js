5948e06cc39d2fdcc9e72a02c2b74a016df4b8fa
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-03-26 09:03:27 -0400

    Red

---
+++ homework-10.1.js
@@ -0,0 +1,22 @@
+var assert = require("assert");
+
+/**
+ * testing subtraction():
+ * one | two | out
+ * ====+=====+====
+ *  +1 | +1  | +0
+ *  +1 | +0  | +1
+ *  +1 | -1  | +2
+ *  -1 | +1  | -2
+ *  -1 | +0  | -1
+ *  -1 | -1  | -0
+ *  +0 | +1  | -1
+ *  +0 | +0  | +0
+ *  +0 | -1  | +1
+ */
+
+assert.equal( 0,(subtraction(+1,+1), " +1 - +1 = +0");
+
+subtraction(a,b) {
+    return 1;
+}
