f4caca5f188ad64ab4c86dd35207d0dd32998bf2
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-03 18:17:55 -0400

    green state

--- homework-10.1.js
+++ homework-10.1.js
@@ -16,10 +16,10 @@ var assert = require("assert");
  */
  
 
-assert.equal( multiplication( +1,+1), +1, " +1 * +1= +1");
-assert.equal( multiplication( +1,+0), +0, " +1 * +1= +1");
+assert.equal( +1, multiplication(+1,+1), " +1 * +1= +1");
+assert.equal( +0, multiplication(+1,+0), " +1 * +1= +0");
 
 function multiplication( a, b) {
-    return +0;
+    return a*b;
     
 }
