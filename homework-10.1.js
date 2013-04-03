209ead11d33e2384c64e47bae9b921000698d548
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-03 18:20:50 -0400

    refactor

--- homework-10.1.js
+++ homework-10.1.js
@@ -18,8 +18,16 @@ var assert = require("assert");
 
 assert.equal( +1, multiplication(+1,+1), " +1 * +1= +1");
 assert.equal( +0, multiplication(+1,+0), " +1 * +1= +0");
+autoAssert( -1, +1, -1);
+
+function autoAssert( a, b, c) {
+    
+    assert.equal(c, multiplication( a, b), " +1 - -1 = +2");
+    
+}
 
 function multiplication( a, b) {
-    return a*b;
+    
+    return (a * b);
     
 }
