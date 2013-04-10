d4c1e82dd38245612c678f756f090732af947140
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-10 11:17:56 -0400

    done for the day

--- homework-11.1.js
+++ homework-11.1.js
@@ -1,6 +1,40 @@
 var assert = require("assert");
 
-var doors = [1];
+var doors = new Array;
+var i;
+var doorMax = 100;
+var hallway;
+var pass = new Array;
 
-assert(doors[1]);
+hallway = {
+    passes: pass,
+    door: doors
+}
+//create the closed doors
+for (i=0;i<doorMax;i++) {
+    
+    doors[i] = 0;
+}
 
+//open every door
+for (i=0;i<doorMax;i++) {
+    
+    doors[i]=1
+}
+
+//close every other door
+for (i=0;i<doorMax;i+=2) {
+    doors[i] = 0
+}
+
+//examine every other door
+for (i=0;i<doorMax;i+=3) {
+    if( doors[i]===0) {
+        doors[i]=1;
+    } else {
+        doors[i]=0;
+    }
+}
+
+//assert(doors[0]===1);
+console.log(doors)
