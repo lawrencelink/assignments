e8294cedf563df13c10c02b6dad363a67eff4771
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-15 10:28:34 -0400

    red, toggle undefined

---
+++ assignment-12.1-test.js
@@ -0,0 +1,29 @@
+var assert = require("assert"),
+    hundredDoors = require("./assignment-12.1-code.js");
+    
+
+// test: do we have a Hallway?
+assert(hundredDoors.hallway);
+
+// test: do we have Doors in our Hallway?
+assert( hundredDoors.hallway.doors);
+// test: do we have 100 Doors?
+assert( hundredDoors.hallway.doors.length === 10);
+
+// test: are all the Doors "closed"?
+
+hundredDoors.hallway.doors.forEach(function(door){
+        assert.equal(door,0);
+})
+
+// do: PASS 1 -- take a walk down the hallway, change the state of the doors
+assert(hundredDoors.hallway.toggle);
+// test: are all the Doors "open"?
+
+// do: PASS 2 -- take a walk down the hallway, change the state of the doors
+
+// test: is every EVEN Door "closed"? is every ODD Door "open"?
+
+// do: PASS 3 -- take a walk down the hallway, change the state of the doors
+
+// test: does the Hallway match our expected state?
