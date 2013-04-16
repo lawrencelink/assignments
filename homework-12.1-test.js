b87fdd0d2f34c7217a17fc0ce921d7a5344fa850
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-16 08:50:03 -0400

    start a new day

---
+++ homework-12.1-test.js
@@ -0,0 +1,40 @@
+var assert = require("assert"),
+    hundredDoors = require("./assignment-12.1-code.js");
+    
+
+// test: do we have a Hallway?
+
+
+// test: do we have Doors in our Hallway?
+
+
+
+// test: do we have 100 Doors?
+
+
+
+// test: are all the Doors "closed"?
+
+
+
+// do: PASS 1 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: are all the Doors "open"?
+
+
+
+// do: PASS 2 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: is every EVEN Door "closed"? is every ODD Door "open"?
+
+
+
+// do: PASS 3 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: does the Hallway match our expected state?
