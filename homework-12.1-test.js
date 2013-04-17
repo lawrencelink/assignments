0d670a233a9634a25317f5202a3e30acc3645b9b
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-17 08:43:17 -0400

    brand new day again

--- homework-12.1-test.js
+++ homework-12.1-test.js
@@ -4,25 +4,23 @@ var assert = require("assert"),
 
 // test: do we have a Hallway?
 
-assert( hundredDoors.hallway);
+
 
 // test: do we have Doors in our Hallway?
 
-assert( hundredDoors.hallway.doors);
+
 
 // test: do we have 100 Doors?
 
-assert( hundredDoors.hallway.doors.length === 10);
+
 
 // test: are all the Doors "closed"?
 
-hundredDoors.hallway.doors.forEach(function(door) {
-    assert.equal( door,0); 
-});
+
 
 // do: PASS 1 -- take a walk down the hallway, change the state of the doors
 
-assert(hundredDoors.hallway.toggle(0),1);
+
 
 // test: are all the Doors "open"?
 
