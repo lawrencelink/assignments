66f73f328eff2a76d30e8826d4a053fd34fa87f6
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-22 09:10:02 -0400

    red, no hallway

---
+++ homework-13.1-test.js
@@ -0,0 +1,41 @@
+var assert = require("assert"),
+    hundredDoors = require("./homework-13.1-code.js");
+    
+
+// test: do we have a Hallway?
+
+assert(hundredDoors.hallway);
+
+// test: do we have Doors in our Hallway?
+
+
+
+// test: do we have 10 Doors?
+
+
+
+// test: are all the Doors "closed"?
+
+
+
+// do: PASS 1 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: are all the Doors "open"?
+
+
+
+// do: PASS 2 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: is every EVEN Door "closed"? is every ODD Door "open"?
+
+
+// do: PASS 3 -- take a walk down the hallway, change the state of the doors
+
+
+
+// test: does the Hallway match our expected state?
+
