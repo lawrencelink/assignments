021e74f1dd42c94a9137d87681652e567a8e5a5d
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-24 09:11:14 -0400

    brand new day

--- homework-13.1-test.js
+++ homework-13.1-test.js
@@ -4,62 +4,37 @@ var assert = require("assert"),
 
 // test: do we have a Hallway?
 
-assert( hundredDoors.hallway);
+
 
 // test: do we have Doors in our Hallway?
 
-assert( hundredDoors.hallway.doors);
+
 
 // test: do we have 10 Doors?
 
-assert( hundredDoors.hallway.doors.length === 10);
+
 
 // test: are all the Doors "closed"?
 
-hundredDoors.hallway.doors.forEach( function( door) {
-    
-   assert.equal( door, 0);
-   
-});
+
 
 // do: PASS 1 -- take a walk down the hallway, change the state of the doors
 
-assert( hundredDoors.hallway.toggle);
+
 
 // test: are all the Doors "open"?
 
-hundredDoors.hallway.doors.forEach( function(door) {
-   
-   assert.equal( hundredDoors.hallway.toggle(door), 1);
-   
-});
+
 
 // do: PASS 2 -- take a walk down the hallway, change the state of the doors
 
-assert( hundredDoors.hallway.pass);
+
 
 // test: is every EVEN Door "closed"? is every ODD Door "open"?
-var tempDoors;
-tempDoors = hundredDoors.hallway.pass();
-console.log( tempDoors);
 
-for (var i = 0; i < hundredDoors.hallway.doors.length; i+=2) {
-    
-    assert.equal( tempDoors[i], 0);
-    
-}
-
-
-for (var i = 1; i < hundredDoors.hallway.doors.length; i+=2) {
-    
-    assert.equal( tempDoors[i], 1);
-    
-}
 
 // do: PASS 3 -- take a walk down the hallway, change the state of the doors
-var tempDoors2;
-tempDoors2 = hundredDoors.hallway.pass(2);
-console.log( tempDoors2);
+
 
 
 // test: does the Hallway match our expected state?
