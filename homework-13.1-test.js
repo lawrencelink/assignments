17956358f981424f4ca430818f314140fb0dd43a
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-25 11:50:57 -0400

    red, no hallway

--- homework-13.1-test.js
+++ homework-13.1-test.js
@@ -1,3 +1,32 @@
 var assert = require("assert"),
     hundredDoors = require("./homework-13.1-code.js");
     
+//do we have a hallway?
+assert( hundredDoors.hallway);
+
+//do we have doors in the hallway?
+
+
+//do we have 10 doors?
+
+
+//are all the doors closed?
+
+
+//can we change the state of a door(create toggle function)?
+
+
+// can we change closed to open?
+
+
+// can we change open to closed?
+
+
+//are all doors in the correct state after first pass?
+
+
+
+//can we tell which pass we are on?
+
+
+//are all the doors in the correct state after each pass?
