f25fc0384c23d30dda1993258d7941952d31e3f7
Lawrence Link <lawrencelinkbellini@gmail.com>
2013-04-25 14:13:13 -0400

    red, no toggle function

--- homework-13.1-test.js
+++ homework-13.1-test.js
@@ -12,11 +12,11 @@ assert( hundredDoors.hallway.doors.length === 10);
 
 //are all the doors closed?
 hundredDoors.hallway.doors.forEach( function(door) {
-    assert.equal( door,1);    
+    assert.equal( door,0);    
 });
 
 //can we change the state of a door(create toggle function)?
-
+assert( hundredDoors.hallway.toggle);
 
 // can we change closed to open?
 
